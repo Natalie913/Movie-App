31496018e891bd8a3b27b5b9c01538bf57f2041b
Natalie <kennilemieux@gmail.com>
2025-03-04 13:25:43 -0500

    updated API Key in env local storage

--- src/components/Switch.jsx
+++ src/components/Switch.jsx
@@ -12,7 +12,7 @@ export default function Switch() {
         <div className='flex col-span-1 justify-end'>
             <button
             type='button'
-            ClassName='flex items-center p-2 mr-2 font-medium  text-gray bg-white rounded-lg border-gray-200 hover:bg-gray-100 dark:text-gray-400 dark:border-600 dark:hover:text-white dark:hover:bg-gray-700'
+            className='flex items-center p-2 mr-2 font-medium  text-gray bg-white rounded-lg border-gray-200 hover:bg-gray-100 dark:text-gray-400 dark:border-600 dark:hover:text-white dark:hover:bg-gray-700'
         onClick={() => setDarkMode(!darkMode)}
         >
             {darkMode && <GoSun className='w-4 h-4' /> }
